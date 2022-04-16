7551ec50128c6af834a813a970fae09999f23b89
Abdelmounaim <af180902@gmail.com>
2022-04-16 17:05:08 +0200

    added pages

---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_MVC_AirBnB.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult Receipt()
+        {
+            return View();
+        }
+    }
+}

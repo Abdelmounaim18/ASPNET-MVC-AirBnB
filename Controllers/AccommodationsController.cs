f14ae150cd389c7cfa8be1cc84cdfc9e7b26c5e6
Abdelmounaim <af180902@gmail.com>
2022-04-16 13:47:33 +0200

    Add project files.

---
+++ Controllers/AccommodationsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_MVC_AirBnB.Controllers
+{
+    public class AccommodationsController : Controller
+    {
+        public IActionResult Index()    
+        {
+            return View();
+        }
+
+        public IActionResult MoreInfo()
+        {
+            return View();
+        }
+
+        public IActionResult Lookup()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_MVC_AirBnB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
